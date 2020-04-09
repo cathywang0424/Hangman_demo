e8d9af6df1df1e23cda910930b10720db5648c06
cathywang0424 <62360925+cathywang0424@users.noreply.github.com>
2020-04-09 15:13:20 -0700

    Update hangman_app-checkpoint.py

--- pa06_hangman ori pack/.ipynb_checkpoints/hangman_app-checkpoint.py
+++ pa06_hangman ori pack/.ipynb_checkpoints/hangman_app-checkpoint.py
@@ -3,12 +3,15 @@
    it is also used as a module in the hangman_webapp flask app
 """
 
+import random
+randomWords = "apple banana mango strawberry orange grape pineapple apricot lemon coconut watermelon cherry papaya berry blueberry blackberry peach lychee muskmelon".split()
+
 def generate_random_word():
-   """ read a list of words from a file and pick a random one to return """
-   return "unimplemented"
+    """ read a list of words from a file and pick a random one to return """
+    return random.choice(randomWords)
 
 def play_hangman():
-   """ this is the python script version of the game """
+   """ this is the python script version of the game, change the part with word into: word = generate_random_word() """
    print("The hangman app is under construction. Try again later!")
 
 if __name__ == '__main__':
