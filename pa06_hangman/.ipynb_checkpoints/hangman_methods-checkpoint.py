0f62465c5346598fe41546879d9c147b21377c60
cathywang0424 <62360925+cathywang0424@users.noreply.github.com>
2020-04-13 15:29:39 -0700

    changes to hangman_methods

--- pa06_hangman/.ipynb_checkpoints/hangman_methods-checkpoint.py
+++ pa06_hangman/.ipynb_checkpoints/hangman_methods-checkpoint.py
@@ -5,71 +5,65 @@
 def play_hangman():
     
     want_to_play = input("Do you want to play the game? Please enter y or n.")
-    
-if want_to_play=="y":
-    want_to_play=True
-    print("Welcome to Hangman! The word you will be guessing is a fruit.")
-if want_to_play=="n":
-    want_to_play=False
-        
-while(want_to_play):
-    word= generate_random_word()
-    guessed_letters = []
-    guesses_left = 6
-    letter = input("Enter a letter:")
-    length = len(word)
-        
-    done = False
-    while not done:
-        if len(letter)>=2 or len(letter) == 0:
-            guesses_left = guesses_left - 1
-            print("Please enter one letter at a time")
-            print("These are the letters you have guessed: ", guessed_letters)
-            print("you have", guesses_left, "guesses left.")
-                
-        elif letter in guessed_letters:
-            guesses_left = guesses_left - 1
-            print("You already guessed that letter.")
-            print_word(word, guessed_letters)
-            print("These are the letters you have guessed: ", guessed_letters)
-            print("you have", guesses_left, "guesses left.")
-                
-        elif letter not in word:
-            guesses_left = guesses_left - 1
-            guessed_letters.append(letter)
-            print("The letter you guessed is not in the word.")
-            print_word(word, guessed_letters)
-            print("These are the letters you have guessed: ", guessed_letters)
-            print("you have", guesses_left, "guesses left.")
-            
-        else:
-            guessed_letters.append(letter)
-            length = length-1
-            print("The letter you guessed is in the word.")
-            print_word(word, guessed_letters)
-            print("These are the letters you have guessed: ", guessed_letters)
-            print("you have", guesses_left, "guesses left.")
-                
-        if length == 0:
-            done = True
-            print("You won the game! The word is ", word)
-            want_to_play=input("Do you want to play another round? Please enter y or n.")
-            if want_to_play=="y":
-                want_to_play=True
-            if want_to_play=="n":
-                want_to_play=False
-                
-        elif guesses_left == 0:
-            done = True
-            print("You lost, the word is ", word)
-            want_to_play=input("Do you want to play another round? Please enter y or n.")
-            if want_to_play=="y":
-                want_to_play=True
-            if want_to_play=="n":
-                want_to_play=False
-        
-        else:
-            letter = input("Please guess a letter: ")
+
+    while want_to_play == "y":
+        word= generate_random_word()
+        guessed_letters = []
+        guesses_left = 6
+        letter = input("Enter a letter:")
+        length = len(word)
+
+        done = False
+        while not done:
+            if len(letter)>=2 or len(letter) == 0:
+                guesses_left = guesses_left - 1
+                print("Please enter one letter at a time")
+                print("These are the letters you have guessed: ", guessed_letters)
+                print("you have", guesses_left, "guesses left.")
+
+            elif letter in guessed_letters:
+                guesses_left = guesses_left - 1
+                print("You already guessed that letter.")
+                print_word(word, guessed_letters)
+                print("These are the letters you have guessed: ", guessed_letters)
+                print("you have", guesses_left, "guesses left.")
+
+            elif letter not in word:
+                guesses_left = guesses_left - 1
+                guessed_letters.append(letter)
+                print("The letter you guessed is not in the word.")
+                print_word(word, guessed_letters)
+                print("These are the letters you have guessed: ", guessed_letters)
+                print("you have", guesses_left, "guesses left.")
+
+            else:
+                guessed_letters.append(letter)
+                length = length-1
+                print("The letter you guessed is in the word.")
+                print_word(word, guessed_letters)
+                print("These are the letters you have guessed: ", guessed_letters)
+                print("you have", guesses_left, "guesses left.")
+
+            if length == 0:
+                done = True
+                print("You won the game! The word is ", word)
+                want_to_play=input("Do you want to play another round? Please enter y or n.")
+                if want_to_play=="y":
+                    want_to_play=True
+                if want_to_play=="n":
+                    want_to_play=False
+
+            elif guesses_left == 0:
+                done = True
+                print("You lost, the word is ", word)
+                want_to_play=input("Do you want to play another round? Please enter y or n.")
+                if want_to_play=="y":
+                    want_to_play=True
+                if want_to_play=="n":
+                    want_to_play=False
+
+            else:
+                letter = input("Please guess a letter: ")
 
 
 
